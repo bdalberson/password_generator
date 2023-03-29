413b71005fd0df6ed8571266a4cec996be35cb6a
Brian <brian@computers-MacBook-Pro.local>
2023-03-29 09:14:55 -0700

    got prompt button working

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,15 @@
 // Assignment code here
+// go through the user stories and make promopts
+// make a Class that stores all the options that the user chooses during the prompts
+// at the end the class will be used to generate the password based on the users selections.  
+
+const password_criteria = {
+  length_defined : 8,
+  lowercase_defined : false,
+  uppercase_defined : false,
+  numeric_defined : false, 
+  special_characters_defined : false
+};
 
 
 // Get references to the #generate element
@@ -15,3 +26,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function get_length(){
+length_defined = prompt("Please enter a length between 8-120");
+console.log(password_criteria.length_defined)
+}
